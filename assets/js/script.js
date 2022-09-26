58719bfbbb34d660695afd3e736736fc9c2c0544
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-09-26 18:46:48 -0400

    Function showing most current data

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,3 +46,26 @@ var showSearchedCities = function(city) {
 
 }
 
+// This will show the most current data
+var showCurrentData = function(city, data) {
+
+    // This will show current data for the following 
+    var tempCurrent = Math.round(data.current.temp);
+    var humidity = Math.round(data.current.humidity);
+    var windSpeed = data.current.wind_speed;
+    var uvIndex = data.current.uvi;
+    var iconCurrent = data.current.weather[0].icon;
+
+    containerEl.textContent = ""
+    containerEl.setAttribute("class", "m-3 border col-10 text-center")
+    var divCityHeader = document.createElement("div")
+    var headerCityDate = document.createElement("h2");
+    var currentdate = moment().format("L");
+    var imageIcon = document.createElement("img");
+    imageIcon.setAttribute('src', "") 
+    imageIcon.setAttribute('src', "https://openweathermap.org/img/wn/" + iconCurrent + "@2x.png")
+    headerCityDate.textContent = city + "   (" + currentdate + ")";
+
+    divCityHeader.appendChild(headerCityDate)
+    divCityHeader.appendChild(imageIcon)
+    currentContainerEl.appendChild(divCityHeader)
